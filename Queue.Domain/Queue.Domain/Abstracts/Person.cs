95aa4df843945442dbbf791b0d06bda655bfb0d7
azamatmavlonov <119999901+azamatmavlonov@users.noreply.github.com>
2023-03-13 12:02:49 +0500

    Added folders(requests, responses, validations, mappers). DTO, FluentValidation, AutoMappers.Dependency Injection. Connection to Npgsql. Adding services, interfaces, models, repository.Добавьте файлы проекта.

--- Queue.Domain/Queue.Domain/Abstracts/Person.cs
+++ Queue.Domain/Queue.Domain/Abstracts/Person.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Queue.Domain.Models;
 
-namespace Queue.Domain.Models
+namespace Queue.Domain.Abstract
 {
     public abstract class Person : EntityBase
     {
--- Queue.Domain/Queue.Domain/Models/Worker.cs
+++ Queue.Domain/Queue.Domain/Models/Worker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Queue.Domain.Abstract;
 using Queue.Domain.Enums;
 
 namespace Queue.Domain.Models
