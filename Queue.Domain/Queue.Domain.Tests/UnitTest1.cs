26f4fef2cf7944a38c8b790342be24d4e9ac59bf
azamatmavlonov <119999901+azamatmavlonov@users.noreply.github.com>
2023-03-13 12:02:49 +0500

    Добавьте файлы проекта.

---
+++ Queue.Domain/Queue.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Queue.Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
